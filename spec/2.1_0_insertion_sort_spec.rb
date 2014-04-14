220fef0f6ce2f482754707a164582a4c35ebf61f
yuanhang <yuanhang.guo@gmail.com>
2014-04-14 21:38:05 +0800

    add more cases for already sorted context

--- spec/2.1_0_insertion_sort_spec.rb
+++ spec/2.1_0_insertion_sort_spec.rb
@@ -1,11 +1,25 @@
 require 'spec_helper'
 require_relative '../2.1_0_insertion_sort'
 
+def assert_sort array
+  InsertionSort.sort(array).should == array.sort
+end
+
 describe InsertionSort do
-  context "numbers" do 
+  context "already sorted" do 
     it "one number will already be sort" do
       array = [1] 
-      InsertionSort.sort(array).should == array.sort
+      assert_sort array
+    end
+    
+    it "empty array" do
+      array = []
+      assert_sort array
+    end
+
+    it "sorted numbers" do
+      array = [1, 2, 3, 4, 5, 5, 6, 7]
+      assert_sort array
     end
   end
 end
